e22f7c2b04e076e03eded02f47f2e1bc6a25cbcc
Liu7 <liuqi0702@126.com>
2023-05-04 13:32:57 +0800

    绘制一个点！

--- helloPoint1.js
+++ helloPoint1.js
@@ -7,6 +7,30 @@ const VSHADER_SOURCE = `
 
 const FSHADER_SOURCE = `
   void main() {
-    
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
   }
 `
+
+function main() {
+  const canvas = document.getElementById('webgl')
+
+  const gl = getWebGLContext(canvas)
+  if (!gl) {
+    console.log('webgl is not suportted!')
+    return 
+  }
+
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Init shaders failed')
+    return
+  }
+
+  // 设置canvas背景色
+  gl.clearColor(0.0, 0.0, 1.0, 1.0)
+
+  // 清空canvas
+  gl.clear(gl.COLOR_BUFFEER_BIT)
+
+  // 绘制一个点
+  gl.drawArrays(gl.POINTS, 0, 1)
+}
